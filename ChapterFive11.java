ec36c6651a748e048df9c2424ef7d511a96aeac1
Beny <benynouba@gmail.com>
2025-12-17 17:30:09 +0100

    for loop

--- ChapterFive11.java
+++ ChapterFive11.java
@@ -7,7 +7,22 @@ public class ChapterFive11{
     System.out.println("How amany times should the application run: ");
     int times = userInput.nextInt();
 
-    
+    System.out.print("Enter your number: ");
+    int smallest = userInput.nextInt();
+
+    int largest = smallest;
+    for(int count = 1; count<times; count++){
+        System.out.print("Enter your number: ");
+        int input = userInput.nextInt();
+
+        if(input<smallest){
+            smallest = input;
+        }
+        if(input>largest){
+            largest = input;
+        }
+    }
+    int sum = smallest+largest;
     
     }
 
