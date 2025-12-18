487ae70ff00c96f3e0cecec33c02d389353d5b36
Beny <benynouba@gmail.com>
2025-12-18 18:14:12 +0100

    asterix reverse

---
+++ Asterix.java
@@ -0,0 +1,12 @@
+public class AsterixTriangle{
+
+    public static void main(String...args){
+
+        for(int count = 0; count <10; count++){
+            System.out.println("");
+            for(int count1 = 9; count1>count ; count1--){
+                System.out.print("*");
+            }
+        }
+    }
+}
---
+++ AsterixTriangle.java
@@ -0,0 +1,12 @@
+public class AsterixTriangle{
+
+    public static void main(String...args){
+
+        for(int count = 1; count <10; count++){
+            System.out.println('*');
+            for(int count1 = 0; count1< count ; count1++){
+                System.out.print('*');
+            }
+        }
+    }
+}
